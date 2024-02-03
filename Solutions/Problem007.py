6ff1c9d3c69a87a35edb54bca19bdfa96864e950
S Ramakrishnan <sramakrishnan1841@gmail.com>
2024-02-03 15:40:31 +0530

    Update Problem007.py

--- Solutions/Problem007.py
+++ Solutions/Problem007.py
@@ -1,7 +1,6 @@
 '''
-QUESTION : By listing the first six prime numbers: 
-    2,3,5,7,11, and 13, we can see that the 6th prime is 13.
-    What is the 10,001st prime number?
+QUESTION : By listing the first six prime numbers: 2,3,5,7,11, and 13, 
+    we can see that the 6th prime is 13. What is the 10,001st prime number?
 
 ANSWER : 104743
 
