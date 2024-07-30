345e70fe239ff0495436a0fa7f57822a02b760a5
S Ramakrishnan <sramakrishnan1841@gmail.com>
2024-07-30 15:48:42 +0530

    Update Problem010.py

--- Solutions/Problem010.py
+++ Solutions/Problem010.py
@@ -4,11 +4,13 @@ QUESTION:
 Summation of Primes - The sum of the primes below 10 is 2+3+5+7 = 17 
 Find the sum of all the primes below two million.
 EXPLANATION: Sieve of Eratosthenes is used to find all the prime numbers below 2 million.
-
 '''
+import time
+start = time.time()
 k = [True]*2000000
 for i in range(2,2000000):
     if k[i]:
         for j in range(2*i,2000000,i):
             k[j] = False
 print(sum([i for i in range(2,2000000) if k[i]]))
+print(time.time()-start)
