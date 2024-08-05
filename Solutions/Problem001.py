999ca8f53d176d3e8f7b24db603bb2cf36cc272d
S Ramakrishnan <sramakrishnan1841@gmail.com>
2024-08-05 22:46:26 +0530

    Update Problem001.py

--- Solutions/Problem001.py
+++ Solutions/Problem001.py
@@ -1,18 +1,12 @@
 '''
 QUESTION : 
-    If we list all the natural numbers below 10 
-    that are multiples of 3 or 5, we get 
-    3,5,6 and 9.The sum of these multiples is 23.
-
-    Find the sum of all the multiples of 3 or 5 
-    below 1000.
+    Find the sum of all the multiples of 3 or 5 below 1000.
 
 ANSWER : 233168
 
 EXPLANATION: 
-    Loop through the numbers below 1000
-    and add the number if it is divisible
-    by either 5 or 3.
+    Loop through the numbers below 1000 and add the 
+    number if it is divisible by either 5 or 3.
     
 '''
 #CODE - QUESTION 1 (PYTHON)
